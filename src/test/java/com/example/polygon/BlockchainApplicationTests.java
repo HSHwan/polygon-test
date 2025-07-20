bc09837280939ee346ee22475bba890685873f81
HSHwan <tnghks2866@gmail.com>
2025-07-20 09:20:58 +0900

    feat: init 설정

---
+++ src/test/java/com/example/polygon/BlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polygon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
