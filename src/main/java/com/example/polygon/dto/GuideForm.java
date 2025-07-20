bd4ded703199d39db4d425769f8e52b6d78b9718
HSHwan <tnghks2866@gmail.com>
2025-07-20 13:56:38 +0900

    feat: guideForm DTO 생성

---
+++ src/main/java/com/example/polygon/dto/GuideForm.java
@@ -0,0 +1,10 @@
+package com.example.polygon.dto;
+
+import lombok.Data;
+
+@Data
+public class GuideForm {
+    private String guideAddress;
+    private String guideId;
+    private String metadata;
+}
